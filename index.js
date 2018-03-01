83fd599d5afc31a4fe3678f2f9810df627be3b15
Benedikt Veith <bv@scherer-software.de>
2018-03-01 15:17:06 +0100

    V0.2.

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var isVoting = false;
 // Load the settings from the config file
 config = JSON.parse(fs.readFileSync("config.json"));
 
+var should_vote_fair = config.should_vote_fair;
+
 steem.api.setOptions({ url: config.steem_node }); // Set Steem Node
 
 if (fs.existsSync('state.json')) {
@@ -39,6 +41,15 @@ function loop() {
     });
 
     if (account) {
+        const votingPower = getVotingPower(account);
+
+        // We have votes and enough power (100% equals 1 Vote every 2.4hrs)
+        if (votingPower >= 100 && outstanding_votes.length > 0 && !isVoting && should_vote_fair) {
+            // Everbody receives same vote weight, independently of bid amount
+            const voteWeigth = 10000 / outstanding_votes.length;
+            sendVotes(voteWeight);
+        }
+
         steem.api.getAccountHistory(account.name, -1, 50, function (err, result) { // Get last 50 transactions of account
             if (err || !result) { // Check for errors
               console.log('Error loading account history: ' + err); // Output error
@@ -50,6 +61,11 @@ function loop() {
 
                 if (trans[0] > trans_number) {
                     if (op[0] == 'transfer' && op[1].to == account.name) { // Check if transaction is transfer TO user account
+
+                        if (config.is_disabled) {
+                            refund(op[1].from, op[1].amount, 'disabled_mode');
+                        }
+
                         if (validUrl.isUri(op[1].memo)) { // Check if memo contains valid URL
                             var amount = op[1].amount;
                             var currency = amount.substr(amount.indexOf(' ') + 1);
@@ -79,10 +95,18 @@ function loop() {
             });
           });
 
-        if (outstanding_votes.length > 0 && !isVoting) {
-            sendVotes();
-        }
+          if (outstanding_votes.length > 0 && !isVoting && !should_vote_fair) {
+              sendVotes();
+          }
     }
+}
+
+function getVotingPower(account) {
+    var secondsago = (new Date - new Date(account.last_vote_time + "Z")) / 1000;
+    var vpow = account.voting_power + (10000 * secondsago / 432000);
+    vpow = Math.min(vpow / 100, 100).toFixed(0);
+
+    return vpow;
 }
 
 function saveState() {
@@ -97,26 +121,32 @@ function saveState() {
   });
 }
 
-function sendVotes() {
+function sendVotes(voteWeight) {
     isVoting = true;
-    vote(outstanding_votes.pop(), 0, function() {
+
+    vote(outstanding_votes.pop(), 0, voteWeigth, function() {
         if (outstanding_votes.length > 0) {
-            setTimeout(function () { sendVotes(); }, 5000);
+            setTimeout(function () { sendVotes(voteWeight); }, 5000);
         } else {
             isVoting = false;
         }
     })
 }
 
-function vote(vote, retries, callback) {
+function vote(vote, retries, voteWeigth, callback) {
     console.log('Voting: ' + vote);
 
-    steem.broadcast.vote(config.private_posting_key, account.name, vote.author, vote.permlink, 10000, function (err, result) {
+    var vote_weight = voteWeigth;
+    if (!should_vote_fair) {
+        vote_weight = 10000;
+    }
+
+    steem.broadcast.vote(config.private_posting_key, account.name, vote.author, vote.permlink, vote_weight, function (err, result) {
         if (err && !result) {
             console.log('Voting failed: ' + err);
 
             if (retries < 1) {
-                vote(vote, retries + 1, callback);
+                vote(vote, retries + 1, voteWeigth, callback);
             } else {
                 refund(vote.author, vote.amount, 'voting_failed');
                 callback();
@@ -125,7 +155,9 @@ function vote(vote, retries, callback) {
             return;
         }
 
-        sendComment(vote);
+        if (config.should_comment) {
+            sendComment(vote);
+        }
 
         if (callback) {
             callback();
